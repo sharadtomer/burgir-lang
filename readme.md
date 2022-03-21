60d77e5f29f431e247735409e427d1abd138080e
sharad <sharad.tomer@grapecity.com>
2022-03-21 20:22:55 +0530

    initial setup

---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a fun language created as an insider joke based on the burger eating habit of one of my colleague
---
+++ src/cli.ts
@@ -0,0 +1,19 @@
+import config from "../config/config.json";
+import { IOHelper } from "./tools/IOHelper";
+
+export class CLI {
+
+    ioHelper: IOHelper;
+    
+    constructor() {
+        this.ioHelper = new IOHelper();
+    }
+
+    async run(){
+        // update this method with main cli functionality
+        
+    }
+
+    
+
+}
